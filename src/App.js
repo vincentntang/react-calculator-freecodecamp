396eb7e95608c8bb1154c9a18916e8d4642b7589
ACpython <10290348+Kagerjay@users.noreply.github.com>
2018-11-07 17:10:53 -0500

    FirstWorkingCommit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Display from './components/Display';
 import './App.css';
 
 class App extends Component {
@@ -19,6 +20,7 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Display/>
         </header>
       </div>
     );
